e78f10a897325706aca1eb50dde48d77b693a062
GabrielMBoeira <gboeira777@gmail.com>
2020-02-27 01:23:06 -0300

    adicionando arquivos

---
+++ operadores-de-atribuicao.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = 10;
+$b = 5;
+
+//$a = $a + $b;  -> é a mesma coisa que $a += $b;
+$a += $b;
+echo $a;
+
+echo "<br>";
+
+$a -= $b; // $a = $a - $b;
+echo $a;
+
+echo "<br>";
+
+$a *= $b; // $a = $a * $b;
+echo $a;
+
+echo "<br>";
+
+$a /= $b; //$a = $a / $b;
+echo $a;
+
+
+
+
+
+
--- operadores-incremento-decremento.php
+++ operadores-incremento-decremento.php
@@ -1,3 +1,27 @@
 <?php
 
-echo timezone_offset_get;
+//pré incremento
+$valor = 20;
+echo ++$valor; //ele incrementou para depois mostrar o valor. 
+echo "<br>";
+
+
+//pós incremento
+$value = 20;
+echo $value++; //ele mostrou o valor para depois incrementar. 
+echo "<br>";
+
+
+
+//pré decremento
+$val = 10;
+echo --$val; // ele decrementa e dpois mostra o valor.
+echo "<br>";
+
+//pós decremento
+$valr = 10;
+echo $valr--; //ele mostra o valor para depois decrementar.
+
+
+
+
