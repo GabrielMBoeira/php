d866b4a9d319ee6fd2437163bdfb1fe731d541fb
GabrielMBoeira <gboeira777@gmail.com>
2020-02-27 02:11:38 -0300

    adicionando arquivos

---
+++ operadores-de-comparacao.php
@@ -0,0 +1,30 @@
+<?php
+
+
+//Operadores de comparação
+
+// == igual
+// != não é igual
+// === estritamente igual
+// !== estritamente diferente
+// <> diferente
+// < menor
+// > maior
+// <= menos igual
+// >= maior igual
+// <=> space ship
+
+
+echo var_dump(20 <=> 30); // lado esquerdo menor => -1 
+echo "<br>";
+echo var_dump(20 <=> 15); // lado esquerdo maior => +1
+echo "<br>";
+echo var_dump(20 <=> 20); // se for igual ambos os lado => 0
+echo "<br>";
+
+
+if(10 == "10"):
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
