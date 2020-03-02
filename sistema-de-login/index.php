d369df4573f0a535aa68b5e9f284639e652fc056
GabrielMBoeira <gboeira777@gmail.com>
2020-03-02 00:19:23 -0300

    adicionando arquivos

---
+++ sistema-de-login/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+<?php
+//Conexão 
+require_once "db_connect.php";
+
+//Sessões
+session_start();
+
+//Botão enviar
+    if(isset($_POST['btn-entrar'])) {
+        $erros = array();
+        $login = mysqli_escape_string($connect, $_POST['login']);
+        $senha = mysqli_escape_string($connect, $_POST['senha']);
+
+        if (empty($login) or empty($senha)) {
+            $erros[] = "<li> Senha ou login não estão preenchidos </li>";
+        } else {
+            $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+            $resultado = mysqli_query($connect, $sql);
+            
+            if (mysqli_num_rows($resultado) > 0) {
+                
+                $sql = "SELECT * FROM usuarios WHERE login = $login AND senha = $senha";
+                $resultado = mysqli_query($connect, $sql);
+
+                if (mysqli_num_rows($resultado) == 1) {
+                    $dados = mysqli_fetch_array($resultado);
+
+                } else {
+                    $erros[] = "<li>Usuário e senha não conferem </li>";
+                }
+                
+            } else {
+                $erros[] = "<li>Usuário inexistente</li>";
+            }
+
+        }
+
+    }
+?>
+
+
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+
+<?php
+    if (!empty($erros)) {
+        foreach($erros as $erro) {
+            echo $erro;
+        }
+    }
+?>
+
+<h1>Login</h1>
+
+<hr>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+Login:  <input type="text" name="login"><br>
+Senha: <input type="password" name="senha"><br>
+<button type="submit" name="btn-entrar">Entrar</button>
+</form>
+    
+</body>
+</html>
