5e9ac093988c27d736930ff8abf4a5d9493b1b20
GabrielMBoeira <gboeira777@gmail.com>
2020-03-01 22:05:28 -0300

    adicionando arquivos

--- upload/index.php
+++ upload/index.php
@@ -2,33 +2,42 @@
 <body>
 
 <?php
+  // UPLOADS MULTIPLOS
+
 
 if(isset($_POST['enviar-formulario'])) {
     $formatosPermitidos = array("png", "jpeg", "jpg", "gif");
-    $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+    $quantidadeArquivos = count($_FILES['arquivo']['name']);
+    $contador = 0;
 
-    if(in_array($extensao, $formatosPermitidos)) {
-        $pasta = "arquivos/";
-        $temporario =   $_FILES['arquivo']['tmp_name'];
-        $novoNome = uniqid().".$extensao";
+    while ($contador < $quantidadeArquivos) {
 
-        if(move_uploaded_file($temporario, $pasta.$novoNome)){
-            $mensagem = "Upload feito com sucesso";
+        $extensao = pathinfo($_FILES['arquivo']['name'][$contador], PATHINFO_EXTENSION);
+
+        if(in_array($extensao, $formatosPermitidos)) {
+            $pasta = "arquivos/";
+            $temporario =   $_FILES['arquivo']['tmp_name'][$contador];
+            $novoNome = uniqid().".$extensao";
+    
+            if(move_uploaded_file($temporario, $pasta.$novoNome)){
+                echo "Upload feito com sucesso para $pasta$novoNome <br>";
+            } else {
+                echo "Erro, ao enviar arquivo $temporario <br>";
+            }
         } else {
-            $mensagem = "Erro, não foi possível fazer upload";
+            echo "$extensao não é permitida <br>";   
         }
-    } else {
-        $mensagem = "Formato inválido";   
+
+        $contador++;
     }
-    echo $mensagem;
+
 }
 
 ?>
-
     
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
 
-    <input type="file" name="arquivo"><br>
+    <input type="file" name="arquivo[]" multiple><br>
     <button type="submit" name="enviar-formulario">Enviar</button>
 
 </form>
