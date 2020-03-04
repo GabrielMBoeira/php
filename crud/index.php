199fb097643630bbf21584e9510accd28377e2c5
GabrielMBoeira <gboeira777@gmail.com>
2020-03-04 01:20:44 -0300

    adicionando arquivos

--- crud/index.php
+++ crud/index.php
@@ -11,7 +11,7 @@ include_once 'includes/message.php';
 
 <div class="row">
     <div class="col s12 m6 push-m3">
-    <h3 class="light">Clientes</h3>
+        <h3 class="light">Clientes</h3>
         <table class="striped">
             <thead>
                 <tr>
@@ -21,38 +21,41 @@ include_once 'includes/message.php';
                     <th>Idade: </th>
                 </tr>
             </thead>
-            
+
             <tbody>
-
                 <?php
-                    $sql = "SELECT * FROM clientes";
-                    $resultado = mysqli_query($connect, $sql);
+                $sql = "SELECT * FROM clientes";
+                $resultado = mysqli_query($connect, $sql);
 
-                    while ($dados = mysqli_fetch_array($resultado)):
+                while ($dados = mysqli_fetch_array($resultado)) :
                 ?>
+                    <tr>
+                        <td><?php echo $dados['nome']; ?></td>
+                        <td><?php echo $dados['sobrenome']; ?></td>
+                        <td><?php echo $dados['email']; ?></td>
+                        <td><?php echo $dados['idade']; ?></td>
+                        <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                        <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
 
-                <tr>
-                    <td><?php echo $dados['nome'];?></td>
-                    <td><?php echo $dados['sobrenome'];?></td>
-                    <td><?php echo $dados['email'];?></td>
-                    <td><?php echo $dados['idade'];?></td>
-                    <td><a href="editar.php?id=<?php echo $dados['id'];?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
-                    <td><a href="modal<?php echo $dados['id'];?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
-                </tr>
+                                            <!-- Modal Structure -->
+                    <div id="#modal<?php echo $dados['id']; ?>" class="modal">
+                        <div class="modal-content">
+                            <h4>Opa!</h4>
+                            <h3>Tem certeza que deseja excluir este cliente?</h3>
+                        </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
 
-                  <!-- Modal Structure -->
-  <div id="modal1" class="modal">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-  </div>
+                            <form action="php-action/delete.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $dados['id']; ?>"> 
+                                <button type="submit" name="btn-deletar" class="btn red">Sim, quero deletar</button>
+                            </form>
+                        </div>
+                    </div>
+                    </tr>
 
                 <?php
-                    endwhile;   
+                endwhile;
                 ?>
 
             </tbody>
